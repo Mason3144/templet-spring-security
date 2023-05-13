59c30c199f72362a2e9563bdd82162808364b638
Mason3144 <lts890303@gmail.com>
2023-05-13 23:52:07 +0900

    dtos, exceptionHandlers, controllers and etc for running the application

---
+++ src/test/java/com/templete/HelloSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
